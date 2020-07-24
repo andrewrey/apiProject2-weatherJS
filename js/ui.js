85aff02bf6beb4562844a184287cb5403640dbdc
Andrew Reynolds <andrew_reynolds@mac.com>
2020-07-24 13:43:02 -0700

    completed UI class to add weather info from weather api

--- js/ui.js
+++ js/ui.js
@@ -7,11 +7,18 @@ class UI{
     this.icon = document.getElementById('w-icon');
     this.humidity = document.getElementById('w-humidity');
     this.feelsLike = document.getElementById('w-feels-like');
-    this.dewpoint = document.getElementById('w-dewpoint');
+    this.pressure = document.getElementById('w-pressure');
     this.wind = document.getElementById('w-wind');
   }
 
   paint(weather){
     this.location.textContent = weather.name;
+    this.desc.textContent = weather.weather[0].description;
+    this.string.textContent = `${weather.main.temp} °C`;
+    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
+    this.feelsLike.textContent = `Feels Like: ${weather.main.feels_like}`;
+    this.pressure.textContent = `Dewpoint: ${weather.main.pressure}`;
+    this.wind.textContent = `Wind Speed: ${weather.wind.speed}, Wind Direction: ${weather.wind.deg}`;
+
   }
 }
